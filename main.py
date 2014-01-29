5ab2fdbad85e8521fc9edcf3b7174114470404a0
Matt <matt.sieberg@yahoo.com>
2014-01-28 21:42:55 -0600

    Integrated items

--- main.py
+++ main.py
@@ -1,45 +1,72 @@
 import script
+import os
 currentroom = 0
 rswitch = 0
 
+
 ###ROOM CLASS###
 class room(object):
-    def __init__(self, rtype):
-        self.rtype = rtype
-    def rchoice(self):
-        global rswitch
-        print self.rtype
-        print script.lscriptReg()
-        rchoice = input("> ")
-        if rchoice == 1: # Room choice begins.
-            rswitch = 1
-            roomLogic()
-        
-        
-        
+        def __init__(self, rtype, itemyn, itype):
+                self.rtype = rtype
+                self.itemyn = itemyn
+                self.itype = itype
+        def rchoice(self):
+                global rswitch
+                print self.rtype
+                if self.itemyn == 1:
+                        print "There appears to be a %s in here." % (self.itype)
+                print script.lscriptReg()
+                rchoice = input("> ")
+                if rchoice == 1: # Room choice begins.
+                        rswitch = 1
+                        print "\n" * 100
+                        roomLogic()
+                elif rchoice == 2: # Item description begins.
+                        self.itemlogic()
+        def itemlogic(self):
+                if self.itemyn == 1:
+                        print "\n" * 100
+                        if self.itype == "box":
+                                print script.iscript001()
+                                print "\n" * 3
+                                
+                else:
+                        print "error"
 
 
 ###ROOM DEFINITIONS###
-ex1 = room("test")
-ex2 = room("test2")
+
+ex1 = room(script.rscript001(), 1, "box")
+ex2 = room(script.rscript002(), 1, 1)
+
+
+
+###ITEM DEFINITIONS###
+
 
 
 ###ROOM LOGIC###
+
+
 def roomLogic():
-    global currentroom
-    global rswitch
-    if rswitch == 0: # Room switch is not necessary. Begin room description.
-        if currentroom == 0:
-            ex1.rchoice()
-        else:
-            ex2.rchoice()
-    else: # Room switch is necessary. Begin room switch.
-        rswitch = 0
-        if currentroom == 0:
-            currentroom = 1
-        else:
-            currentroom = 0
+        global currentroom
+        global rswitch
+        if rswitch == 0: # Room switch is not necessary. Begin room description.
+                if currentroom == 0:
+                        ex1.rchoice()
+                else:
+                        ex2.rchoice()
+
+        else: # Room switch is necessary. Begin room switch.
+                rswitch = 0
+                if currentroom == 0:
+                        currentroom = 1
+                else:
+                        currentroom = 0
+
         roomLogic()
+
+
 
 ###BEGIN###
 roomLogic()
--- script.py
+++ script.py
@@ -1,15 +1,35 @@
 ###ROOM DESCRIPTIONS###
 
+
 def rscript001():
-    return "This is the first room. It has grey walls."
+    
+	return "This is the first room. It has grey walls."
+
+
 def rscript002():
-    return "This is the second room. It has blue walls."
+    
+	return "This is the second room. It has blue walls."
+
+
+
+###ITEM DESCRIPTIONS###
+def iscript001():
+	return "It's an item."
+
+
+
 
 ###ROOM LOGIC DESCRIPTIONS###
 
+
+
 def lscriptReg():
-    return """What would you like to do?
+    
+	return """What would you like to do?
+
+
 
 1. Go to next room
+2. Examine item
 """
 
